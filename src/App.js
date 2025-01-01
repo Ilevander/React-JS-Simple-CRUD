5a8c98ec1599b2ab920f2f8790db6f0cb399bd6e
dell <dell@DESKTOP-4P8U8AR>
2025-01-01 15:33:43 +0100

    Customizing App.js for axios api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.css";
+import api from "./api/axiosConfig";
+import { useEffect, useState } from "react";
+import CustomerComponent from "./components/CustomerComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [customers, setCustomers] = useState([]);
+    useEffect(() => {
+          (async () => await load())();
+        }, []);
+      
+      async function load() {
+          const result = await api.get("/all");
+          setCustomers(result.data);
+      }
+      return (
+          <div>
+              <h1 className="text-center">List Of customers</h1>
+              <CustomerComponent load={load} customers={customers} />
+          </div>
+        );
 }
 
 export default App;
--- src/api/axisConfig.js
+++ src/api/axisConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/rest/customer",
+ baseURL: "http://localhost:8080/api/rest/customer",
 });
